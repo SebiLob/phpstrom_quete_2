21d5178fb4bf3477e3d57e9129f32c961ff90d6a
SebiLob <solomon.grundy.51@gmail.com>
2017-03-27 14:32:53 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+$students = array(
+    "Emmanuel" => 42,
+    "Thierry" => 51,
+    "Pascal" => 45,
+    "Eric" => 48,
+    "Nicolas" => 19
+);
+
+echo array_sum($students)/count($students);
+?>
